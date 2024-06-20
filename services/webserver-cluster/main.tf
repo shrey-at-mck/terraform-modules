2e5ec2cd2d1a3bb7b2c9f19d7906ae48d3af7bd0
Shrey Kejriwal <shreykejriwal@gmail.com>
2024-06-20 11:54:48 +0530

    remove complete db data resource

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -6,15 +6,6 @@ locals {
   all_ips      = ["0.0.0.0/0"]
 }
 
-data "terraform_remote_state" "db" {
-  backend = "s3"
-  config = {
-    bucket = var.db_remote_state_bucket
-    key    = var.db_remote_state_key
-    region = "us-east-1"
-  }
-}
-
 data "aws_vpc" "default" {
   default = true
 }
